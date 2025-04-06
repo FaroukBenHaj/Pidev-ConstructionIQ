d9c7cf60c094ad54ff5c9f534ebf11a3606fc900
FaroukBenhaj <mohamedfarouk.benhajamor@esprit.tn>
2025-04-06 22:11:27 +0100

    correction du guard et interceptor

---
+++ Frontend/FrontOffice_ConstructionIQ/src/app/services/guard/auth.guard.ts
@@ -0,0 +1,17 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {KeycloakService} from "../keycloak/keycloak.service";
+import {inject} from "@angular/core";
+
+//protects routes by checking authentication status before allowing access
+
+export const authGuard: CanActivateFn = (route, state) => {
+  //Checks if the user's token is expired
+  const  keycloakService : KeycloakService = inject(KeycloakService);
+  const router: Router = inject(Router);
+  if(keycloakService.Keycloak?.isTokenExpired()){
+    router.navigate(['login']);
+    return false;
+  }
+  return true;
+};
+// this function is a standalone fn .
