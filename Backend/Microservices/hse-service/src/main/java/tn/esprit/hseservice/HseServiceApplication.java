cf72a5d660edc6fe23f69ad369737cdee1d976de
maissahasni <101124164+maissahasni@users.noreply.github.com>
2025-04-17 02:30:30 +0100

    link my service to eureka

--- Backend/Microservices/hse-service/src/main/java/tn/esprit/hseservice/HseServiceApplication.java
+++ Backend/Microservices/hse-service/src/main/java/tn/esprit/hseservice/HseServiceApplication.java
@@ -2,8 +2,10 @@ package tn.esprit.hseservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HseServiceApplication {
 
     public static void main(String[] args) {
